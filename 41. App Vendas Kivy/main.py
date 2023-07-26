de3d8e20bd632a6ea86cf7166740b2593dd50940
Murilo Fabricio <murilo_fabricio@hotmail.com>
2023-07-26 19:04:41 -0300

    Aula8_Gerenciador_Telas

--- 41. App Vendas Kivy/main.py
+++ 41. App Vendas Kivy/main.py
@@ -1,6 +1,15 @@
+#IMPORTANDO TODAS AS BIBLIOTECAS QUE SERÃO UTILIZADAS
 from kivy.app import App
 from kivy.lang import Builder
+from kivy.uix.screenmanager import Screen
 
+
+#DECLARANDO TODAS AS TELAS QUE SERÃO GERENCIADAS NO APP
+class Home_Page(Screen):
+    pass
+
+
+#FUNÇÃO PRINCIPAL QUE RODA O APP
 GUI = Builder.load_file("main.kv")
 
 class MAinApp(App):
