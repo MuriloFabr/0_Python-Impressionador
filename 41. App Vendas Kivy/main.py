c1f9c442b0eb7e3420b67f2361d284150c818800
Murilo Fabricio <murilo_fabricio@hotmail.com>
2023-09-01 00:08:19 -0300

    Aula23_Criando_Banner_Vendas

--- 41. App Vendas Kivy/main.py
+++ 41. App Vendas Kivy/main.py
@@ -3,6 +3,7 @@ from kivy.app import App
 from kivy.lang import Builder
 from telas import *
 from botoes import *
+from funcoes import *
 import requests
 
 
@@ -17,18 +18,38 @@ class MainApp(App):
     
 
     def on_start(self):
+
+        ### Pegando o Dicionário de Dados de determinado usuário
         #requisicao = requests.get("https://hashtag-app-vendas-default-rtdb.firebaseio.com/.json") #sempre colocar .json no final do link
         #print(requisicao.json()) #essencialmente a requisição vira um arquivo .json do python. Aqui está puxando o banco inteiro ainda
-
         requisicao = requests.get(f"https://hashtag-app-vendas-default-rtdb.firebaseio.com/{self.id_usuario}.json") #olhar módulo de Orientação a objetos para entender porque chamar a variável com o self
         #print(requisicao.json()) #aqui está puxando apenas o dicionário do usuário pretendido
         requisicao_dic = requisicao.json()
         #print(requisicao_dic)
+
+        ### Preencher Foto de Perfil
         avatar = requisicao_dic["avatar"]
         #print(avatar)
         #print(self.root.ids) #todos os ids que existem no nosso arquivo main do programa
         foto_superior = self.root.ids["foto_superior"]
         foto_superior.source = f"icones/fotos_perfil/{avatar}"
+
+        ### Preencher Lista de Vendas
+        try:
+            vendas = requisicao_dic["vendas"][1:]
+            for venda in vendas:
+                # print(venda)
+                banner = BannerVenda(cliente=venda["cliente"], foto_cliente=venda["foto_cliente"],
+                                     produto=venda["produto"], foto_produto=venda["foto_produto"],
+                                     data=venda["data"], preco=venda["preco_total"],
+                                     unidade=venda["unidade"], quantidade=venda["quantidade"])
+                pagina_homepage = self.root.ids["home_page"]
+                # print(pagina_homepage.ids)
+                lista_vendas = pagina_homepage.ids["lista_vendas"]
+                lista_vendas.add_widget(banner)
+        except:
+            pass
+
     
 
     def mudar_tela(self,id_tela):
